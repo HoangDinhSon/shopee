8e0c7f766da7ba69ad6446bb2f8b32e2ae1b7df1
nguyenthithuky <nguyenthithuky151069@gmail.com>
2024-03-26 03:17:00 +0700

    enable sourcemap and fix error link import use src

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3000
+  },
+  //  khi không có thì devSourceMap thì tren dev tool của trình duyệt không cho biết cái css nằm o file nào , nó chỉ hiện style check vite document
+  // vite> config>Shared Options> devSourcemap
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src')
+    }
   }
 })
